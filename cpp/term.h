15dc142928fcafef4edcad73d77778ad79e551d5
Mathias Ottenbreit <mathiasottenbreit@gmail.com>
2022-06-17 18:07:19 +0200

    limitation on adding second last bin

--- cpp/term.h
+++ cpp/term.h
@@ -396,12 +396,15 @@ void Term::setup_bins()
                     size_t observations_between_outer_start_indexes{potential_start_indexes[last_potential_start_index]-potential_start_indexes[0]};
                     size_t bins_to_create{bins-2};
                     size_t desired_observations_in_bin{std::max((observations_between_outer_start_indexes)/bins_to_create+1, static_cast<size_t>(1))};
+                    size_t desired_observations_in_second_last_bin{desired_observations_in_bin*3/4};
                     size_t index_of_start_index_for_previous_bin{0};
                     size_t distance;
+                    size_t distance_to_end;
                     for (size_t index_of_start_index = 1; index_of_start_index < last_potential_start_index-1; ++index_of_start_index)
                     {
                         distance = potential_start_indexes[index_of_start_index]-potential_start_indexes[index_of_start_index_for_previous_bin];
-                        bool can_add_bin{distance>=desired_observations_in_bin};
+                        distance_to_end = potential_start_indexes[last_potential_start_index]-potential_start_indexes[index_of_start_index];
+                        bool can_add_bin{distance>=desired_observations_in_bin && distance_to_end>=desired_observations_in_second_last_bin};
                         if(can_add_bin)
                         {
                             bins_start_index.push_back(potential_start_indexes[index_of_start_index]);
