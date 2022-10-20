7e743ade9dfc789ad46200a748d1bf08225267f0
Mathias Ottenbreit <mathiasottenbreit@gmail.com>
2022-10-20 22:16:16 +0200

    gamma test

--- cpp/test ALRRegressor gamma.cpp
+++ cpp/test ALRRegressor gamma.cpp
@@ -52,7 +52,7 @@ int main()
     save_data("data/output.csv",predictions);
 
     std::cout<<predictions.mean()<<"\n\n";
-    tests.push_back(check_if_approximately_equal(predictions.mean(),23.7191,0.00001));
+    tests.push_back(check_if_approximately_equal(predictions.mean(),23.7308,0.00001));
 
     //std::cout<<model.validation_error_steps<<"\n\n";
 
