130c52a8b0fd984565c4b5a673e753ac9efa470f
mathias-von-ottenbreit <ottenbreitdatascience@gmail.com>
2024-07-17 14:50:20 +0200

    wip

--- python/benchmarks.py
+++ python/benchmarks.py
@@ -4,8 +4,11 @@ import joblib
 xgboost_ver = "2.1.0"
 try:
     import xgboost
+
     if xgboost.__version__ != xgboost_ver:
-        raise Exception(f"To preserve rank ordering we require xgboost=={xgboost_ver}, but have xgboost=={xgboost.__version__}")
+        raise Exception(
+            f"To preserve rank ordering we require xgboost=={xgboost_ver}, but have xgboost=={xgboost.__version__}"
+        )
 except ModuleNotFoundError:
     pass
     #!pip install -U --quiet xgboost=={xgboost_ver}
@@ -29,14 +32,15 @@ except ModuleNotFoundError:
 # %%
 
 try:
-    completed_so_far=joblib.load("completed_so_far.zip")
-except: 
-    completed_so_far=set()
+    completed_so_far = joblib.load("completed_so_far.zip")
+except:
+    completed_so_far = set()
+
 
 def trial_filter(task):
     min_samples = 1
     max_samples = 1000000000000
-    
+
     if task.scalar_measure("n_rows") < min_samples:
         return []
 
@@ -45,12 +49,12 @@ def trial_filter(task):
 
     if task.origin == "openml":
         exclude_set = set()
-#        exclude_set = set(['isolet', 'Devnagari-Script', 'CIFAR_10'])
-#        exclude_set = set([
-#            'Fashion-MNIST', 'mfeat-pixel', 'Bioresponse',
-#            'mfeat-factors', 'isolet', 'cnae-9', "Internet-Advertisements",
-#            'har', 'Devnagari-Script', 'mnist_784', 'CIFAR_10',
-#        ])
+        #        exclude_set = set(['isolet', 'Devnagari-Script', 'CIFAR_10'])
+        #        exclude_set = set([
+        #            'Fashion-MNIST', 'mfeat-pixel', 'Bioresponse',
+        #            'mfeat-factors', 'isolet', 'cnae-9', "Internet-Advertisements",
+        #            'har', 'Devnagari-Script', 'mnist_784', 'CIFAR_10',
+        #        ])
         if task.name in exclude_set:
             return []
     elif task.origin == "pmlb":
@@ -62,14 +66,14 @@ def trial_filter(task):
             pass  # include PMLB regression datasets
         else:
             raise Exception(f"Unrecognized task problem {task.problem}")
-        
+
         exclude_set = set()
         if task.name in exclude_set:
             return []
     else:
         raise Exception(f"Unrecognized task origin {task.origin}")
 
-    exclude_set=completed_so_far.copy()
+    exclude_set = completed_so_far.copy()
     if task.name in exclude_set:
         return []
 
@@ -79,14 +83,18 @@ def trial_filter(task):
         "aplr-base",
     ]
 
+
 # %%
 def trial_runner(trial):
-    seed=42
+    seed = 42
     extra_params = {}
     # extra_params = {"interactions":0, "max_rounds":5}
-    
+
     from xgboost import XGBClassifier, XGBRegressor
-    from interpret.glassbox import ExplainableBoostingClassifier, ExplainableBoostingRegressor
+    from interpret.glassbox import (
+        ExplainableBoostingClassifier,
+        ExplainableBoostingRegressor,
+    )
     from aplr import APLRClassifier, APLRRegressor
     from sklearn.metrics import roc_auc_score, root_mean_squared_error, log_loss
     from sklearn.model_selection import train_test_split
@@ -101,21 +109,25 @@ def trial_runner(trial):
 
     # TODO: move this into powerlift
     import pandas as pd
+
     for col_name in X.columns:
         col = X[col_name]
-        if col.dtype.name == 'object':
+        if col.dtype.name == "object":
             X[col_name] = col.astype(pd.CategoricalDtype(ordered=False))
-        elif col.dtype.name == 'category' and col.cat.ordered:
+        elif col.dtype.name == "category" and col.cat.ordered:
             X[col_name] = col.cat.as_unordered()
     import numpy as np
+
     _, y = np.unique(y, return_inverse=True)
 
     stratification = None
     if trial.task.problem in ["binary", "multiclass"]:
         # use stratefied, otherwise eval can fail if one of the classes is not in the training set
         stratification = y
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=stratification, random_state=seed)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, stratify=stratification, random_state=seed
+    )
 
     # Build preprocessor
     is_cat = meta["categorical_mask"]
@@ -142,7 +154,19 @@ def trial_runner(trial):
         elif trial.method.name == "ebm-base":
             est = ExplainableBoostingClassifier(**extra_params)
         elif trial.method.name == "aplr-base":
-            est = Pipeline([("ct", ct), ("est", APLRClassifier(v=0.5, num_first_steps_with_linear_effects_only=400, boosting_steps_before_interactions_are_allowed=500))])
+            est = Pipeline(
+                [
+                    ("ct", ct),
+                    (
+                        "est",
+                        APLRClassifier(
+                            v=0.5,
+                            num_first_steps_with_linear_effects_only=400,
+                            boosting_steps_before_interactions_are_allowed=500,
+                        ),
+                    ),
+                ]
+            )
             y_train = y_train.astype(str)
             y_test = y_test.astype(str)
         else:
@@ -155,7 +179,19 @@ def trial_runner(trial):
         elif trial.method.name == "ebm-base":
             est = ExplainableBoostingRegressor(**extra_params)
         elif trial.method.name == "aplr-base":
-            est = Pipeline([("ct", ct), ("est", APLRRegressor(v=0.5, num_first_steps_with_linear_effects_only=400, boosting_steps_before_interactions_are_allowed=500))])
+            est = Pipeline(
+                [
+                    ("ct", ct),
+                    (
+                        "est",
+                        APLRRegressor(
+                            v=0.5,
+                            num_first_steps_with_linear_effects_only=400,
+                            boosting_steps_before_interactions_are_allowed=500,
+                        ),
+                    ),
+                ]
+            )
         else:
             raise RuntimeError(f"Method unavailable for {trial.method.name}")
 
@@ -168,16 +204,19 @@ def trial_runner(trial):
         global_counter += 1
     except NameError:
         global_counter = 0
-    
+
     # Train
-    print(f"FIT: {global_counter}, {trial.task.origin}, {trial.task.name}, {trial.method.name}, ", end="")
+    print(
+        f"FIT: {global_counter}, {trial.task.origin}, {trial.task.name}, {trial.method.name}, ",
+        end="",
+    )
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore")
         start_time = time()
         est.fit(X_train, y_train)
         elapsed_time = time() - start_time
     trial.log("fit_time", elapsed_time)
-    
+
     # Predict
     start_time = time()
     predictions = predict_fn(X_test)
@@ -185,7 +224,7 @@ def trial_runner(trial):
     trial.log("predict_time", elapsed_time)
 
     if trial.task.problem == "binary":
-        predictions = predictions[:,1]
+        predictions = predictions[:, 1]
 
         eval_score = roc_auc_score(y_test, predictions)
         trial.log("auc", eval_score)
@@ -193,7 +232,9 @@ def trial_runner(trial):
         eval_score2 = log_loss(y_test, predictions)
         trial.log("log_loss", eval_score2)
     elif trial.task.problem == "multiclass":
-        eval_score = roc_auc_score(y_test, predictions, average="weighted", multi_class="ovo")
+        eval_score = roc_auc_score(
+            y_test, predictions, average="weighted", multi_class="ovo"
+        )
         trial.log("multi_auc", eval_score)
 
         eval_score2 = log_loss(y_test, predictions)
@@ -212,27 +253,31 @@ def trial_runner(trial):
     else:
         raise Exception(f"Unrecognized task problem {trial.task.problem}")
 
-    if (trial.method.name=="aplr-base"):
+    if trial.method.name == "aplr-base":
         completed_so_far.add(trial._task.name)
-        joblib.dump(completed_so_far,"completed_so_far.zip",9)
+        joblib.dump(completed_so_far, "completed_so_far.zip", 9)
         try:
-            benchmark_results=joblib.load("benchmark_results_so_far.zip")
-            benchmark_results=pd.concat([benchmark_results,benchmark.results()], ignore_index=True).drop_duplicates(subset=["method","task","name"])
+            benchmark_results = joblib.load("benchmark_results_so_far.zip")
+            benchmark_results = pd.concat(
+                [benchmark_results, benchmark.results()], ignore_index=True
+            ).drop_duplicates(subset=["method", "task", "name"])
         except:
-            benchmark_results=benchmark.results()
-        joblib.dump(benchmark_results,"benchmark_results_so_far.zip",9)
+            benchmark_results = benchmark.results()
+        joblib.dump(benchmark_results, "benchmark_results_so_far.zip", 9)
         try:
             benchmark_results.to_excel("benchmark_results_so_far.xlsx")
         except:
             pass
-        
+
     print(eval_score)
 
+
 # %%
-force_recreate=False
-exist_ok=True
+force_recreate = False
+exist_ok = True
 
 import uuid
+
 experiment_name = "myexperiment" + "__" + str(uuid.uuid4())
 print("Experiment name: " + str(experiment_name))
 
@@ -245,7 +290,7 @@ import os
 # Initialize database (if needed).
 store = Store(f"sqlite:///{os.getcwd()}/powerlift.db", force_recreate=force_recreate)
 
-cache_dir="~/.powerlift"
+cache_dir = "~/.powerlift"
 data_retrieval = chain(
     # retrieve_catboost_50k(cache_dir=cache_dir),
     retrieve_pmlb(cache_dir=cache_dir),
@@ -264,69 +309,98 @@ benchmark.wait_until_complete()
 
 # %%
 # re-establish connection
-#benchmark = Benchmark(conn_str, name=experiment_name)
+# benchmark = Benchmark(conn_str, name=experiment_name)
 
 # %%
 status_df = benchmark.status()
 for errmsg in status_df["errmsg"]:
     if errmsg is not None:
         print("ERROR: " + str(errmsg))
-print(status_df['status'].value_counts().to_string(index=True, header=False))
+print(status_df["status"].value_counts().to_string(index=True, header=False))
 
 # %%
 results_df = benchmark.results()
-results_df.to_csv('results.csv', index=None)
+results_df.to_csv("results.csv", index=None)
 
 # %%
 # reload if analyzing later
 import pandas as pd
+
 results_df = joblib.load("benchmark_results_so_far.zip")
 
 # %%
 import pandas as pd
 
-averages = results_df.groupby(['method', 'name'])['num_val'].mean().unstack().reset_index()
+averages = (
+    results_df.groupby(["method", "name"])["num_val"].mean().unstack().reset_index()
+)
 
-metric_ranks = results_df.pivot_table('num_val', ['task', 'name'], 'method')
-metric_ranks = metric_ranks.rank(axis=1, ascending=True, method='min')
-metric_ranks = metric_ranks.groupby('name').mean().transpose()
+metric_ranks = results_df.pivot_table("num_val", ["task", "name"], "method")
+metric_ranks = metric_ranks.rank(axis=1, ascending=True, method="min")
+metric_ranks = metric_ranks.groupby("name").mean().transpose()
 metric_ranks.columns = [f"{col}_RANK" for col in metric_ranks.columns]
 metric_ranks = metric_ranks.reset_index()
 
-overall_rank = results_df[results_df['name'].isin(['log_loss', 'cross_entropy', 'nrmse'])]
-overall_rank = overall_rank.pivot_table('num_val', 'task', 'method')
-overall_rank = overall_rank.rank(axis=1, ascending=True, method='min')
+overall_rank = results_df[
+    results_df["name"].isin(["log_loss", "cross_entropy", "nrmse"])
+]
+overall_rank = overall_rank.pivot_table("num_val", "task", "method")
+overall_rank = overall_rank.rank(axis=1, ascending=True, method="min")
 overall_rank = overall_rank.mean()
-overall_rank = overall_rank.to_frame(name='RANK').reset_index()
+overall_rank = overall_rank.to_frame(name="RANK").reset_index()
 
-desired_columns = ['method', 'RANK', 'auc', 'multi_auc', 'nrmse', 'log_loss_RANK', 'cross_entropy_RANK', 'nrmse_RANK', 'fit_time', 'predict_time']
-combined_df = averages.merge(metric_ranks, on='method').merge(overall_rank, on='method')
+desired_columns = [
+    "method",
+    "RANK",
+    "auc",
+    "multi_auc",
+    "nrmse",
+    "log_loss_RANK",
+    "cross_entropy_RANK",
+    "nrmse_RANK",
+    "fit_time",
+    "predict_time",
+]
+combined_df = averages.merge(metric_ranks, on="method").merge(overall_rank, on="method")
 combined_df = combined_df.reindex(columns=desired_columns)
-combined_df = combined_df.sort_values(by='RANK')
+combined_df = combined_df.sort_values(by="RANK")
 
 print(combined_df.to_string(index=False))
-combined_df.to_excel('combined_df.xlsx')
+combined_df.to_excel("combined_df.xlsx")
 
 # %%
-desired_columns = ['method', 'RANK', 'auc', 'multi_auc', 'nrmse', 'log_loss', 'cross_entropy', 'fit_time', 'predict_time']
+desired_columns = [
+    "method",
+    "RANK",
+    "auc",
+    "multi_auc",
+    "nrmse",
+    "log_loss",
+    "cross_entropy",
+    "fit_time",
+    "predict_time",
+]
 row_order = combined_df["method"]
-counts = results_df.groupby(['method', 'name']).size().unstack()
+counts = results_df.groupby(["method", "name"]).size().unstack()
 counts = counts.reindex(row_order, axis=0).reset_index()
-counts['RANK'] = counts['log_loss'] + counts['cross_entropy'] + counts['nrmse']
+counts["RANK"] = counts["log_loss"] + counts["cross_entropy"] + counts["nrmse"]
 counts = counts.reindex(columns=desired_columns)
 print(counts.to_string(index=False))
 counts.to_excel("counts.xlsx")
 
 # %%
-fit_times = results_df[results_df['name'] == 'fit_time']
-fit_times = fit_times.pivot_table('num_val', 'task', 'method')
+fit_times = results_df[results_df["name"] == "fit_time"]
+fit_times = fit_times.pivot_table("num_val", "task", "method")
 fit_times = fit_times.dropna()
-fit_times["ratios"] = fit_times['ebm-base'] / fit_times['xgboost-base']
+fit_times["ratios"] = fit_times["ebm-base"] / fit_times["xgboost-base"]
 import numpy as np
-fit_times_deciles = np.percentile(fit_times["ratios"], [90, 80, 70, 60, 50, 40, 30, 20, 10])
+
+fit_times_deciles = np.percentile(
+    fit_times["ratios"], [90, 80, 70, 60, 50, 40, 30, 20, 10]
+)
 fit_times_deciles = [f"{decile:.2f}  " for decile in fit_times_deciles]
-max_ratio= fit_times["ratios"].max()
-min_ratio= fit_times["ratios"].min()
+max_ratio = fit_times["ratios"].max()
+min_ratio = fit_times["ratios"].min()
 print("fit time ratio deciles:")
 print(*fit_times_deciles)
 print(f"max: {max_ratio:.2f}")
