e69aad98fa568f3a4ebcfea0bec97ed9df991d08
Mathias Ottenbreit <mathiasottenbreit@gmail.com>
2024-10-29 20:53:31 +0100

    wip

--- cpp/APLRRegressor.h
+++ cpp/APLRRegressor.h
@@ -2702,6 +2702,8 @@ void APLRRegressor::set_intercept(double value)
 {
     if (model_has_not_been_trained())
         throw std::runtime_error("The model must be trained with fit() before set_intercept() can be run.");
+    if (!std::isfinite(value))
+        throw std::runtime_error("The updated intercept must be finite.");
     intercept = value;
     term_coefficients[0] = value;
 }
